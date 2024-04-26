787ee4aeb12f7cc9f979540ba3eefb34329f6125
Joo Hyoung Cha <aoikazto@naver.com>
2024-04-27 00:50:44 +0900

    nya.

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e ASCODE_USERID=$1 -e ASCODE_USERPW=$2 ghcr.io/asw-dod/response-for-ascode:1.0.0
+docker run -e ASCODE_USERID=$1 -e ASCODE_USERPW=$2 -e DURATION_PER_CHECK=300 ghcr.io/asw-dod/response-for-ascode:1.0.2
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker pull ghcr.io/asw-dod/response-for-ascode:1.0.0
+docker pull ghcr.io/asw-dod/response-for-ascode:1.0.2
 
